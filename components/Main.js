7a49c5954acb7ee800dfc3764dfeb1600b64b0e1
Fabio Coelho <fbclh@protonmail.com>
2022-05-08 15:15:03 -0300

    Refactor components

--- components/Main.js
+++ components/Main.js
@@ -1,22 +1,40 @@
-// import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.css';
 
 export const Main = () => (
   <main className={styles.main}>
+    <h1 className={styles.title}>
+      Welcome to <span>Blogroll.js</span>
+    </h1>
+
+    <p className={styles.description}>
+      A curated list of blog articles about the JS ecosystem and modern Web Dev.
+    </p>
+
     <div className={styles.grid}>
-      <a href="https://nextjs.org/docs" className={styles.card}>
-        {/* <h2>Documentation &rarr;</h2> */}
+      <a
+        className={styles.card}
+        href="https://auth0.com/blog/how-to-manage-javascript-fatigue/"
+        target="_blank"
+        rel="noreferrer"
+      >
         <p>How to Manage JavaScript Fatigue</p>
       </a>
 
-      <a href="https://nextjs.org/learn" className={styles.card}>
-        {/* <h2>Learn &rarr;</h2> */}
-        <p>
-          React Architecture: How to Structure and Organize a React Application
-        </p>
+      <a
+        className={styles.card}
+        href="https://www.taniarascia.com/react-architecture-directory-structure/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <p>How to Structure and Organize a React Application</p>
       </a>
 
-      <a href="https://nextjs.org/learn" className={styles.card}>
-        {/* <h2>Learn &rarr;</h2> */}
+      <a
+        className={styles.card}
+        href="https://www.toptal.com/javascript/comprehensive-guide-javascript-design-patterns"
+        target="_blank"
+        rel="noreferrer"
+      >
         <p>The Comprehensive Guide to JavaScript Design Patterns</p>
       </a>
     </div>
